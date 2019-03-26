e9881540b891bdd8aa3a35cce10dd2e14767efea
Scott <xdon2462@uni.sydney.edu.au>
2019-03-27 10:00:33 +1100

    Set view engine & render HTML page through ejs

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const port = 8080
 // so that it can be accessed and updated in routers
 app.locals.counter = 0;
 
+// Add router file
+require('./router/main')(app);
+
+// Define directory of HTML file
+app.set('views', __dirname + '/views');
+
+// Set view engine & render HTML page through ejs
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 // Run the app on port 8080
 app.listen(port, () => console.log('My app listening on port 8080!'))
 
