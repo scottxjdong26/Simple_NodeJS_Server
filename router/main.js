42d2e4cca952337b754ba8e35075fa4740998b6f
Scott <xdon2462@uni.sydney.edu.au>
2019-03-27 10:16:29 +1100

    Add a GET request with endpoint "/getCounter"

--- router/main.js
+++ router/main.js
@@ -3,4 +3,18 @@ module.exports = function(app)
 	app.get('/', function(req, res, next) {
 		res.render('index.html')
 	});
+	
+	/**
+	 * When the user makes a request to /getCounter,
+	 * it should return the counter as a JSON object along with the current date, ie:
+     * {"date":<timestamp>,"counter":<counter>}.
+	 */
+	app.get('/getCounter', function(req, res, next) {
+		// get the value of the global counter
+		var counter = req.app.locals.counter;
+		// return the counter as a JSON object along with the current date
+		res.json({ date: Date.now(), counter: counter});
+	});
+
+	
 }
